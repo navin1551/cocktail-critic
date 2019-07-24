89cabb1c154928d608e1fe2485887bd13be3a430
Navin Kumar <navin1551@gmail.com>
2019-07-23 18:18:33 -0700

    working on search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Header from "./components/Header/Header";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Register from "./components/Register/Register";
+import Search from "./components/Search/Search";
 
 export default class App extends React.Component {
+  state = {
+    cocktails: [],
+    reviews: [],
+    searchResults: []
+  };
+
   render() {
     return (
-      <div>
-        <p>Cocktail Critic</p>
+      <div className="app">
+        <section>
+          <NavBar />
+          <Header />
+        </section>
+        <main>
+          <Switch>
+            <Route exact path="/" component={AboutUs} />
+            <Route path="/register" component={Register} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </main>
       </div>
     );
   }
