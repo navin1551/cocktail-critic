86b34123f97c9438efdfd332ad9ef7ddc2ac01ff
Navin Kumar <navin1551@gmail.com>
2019-07-29 10:15:32 -0700

    trying to fix 502 error

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://nameless-taiga-57862.herokuapp.com",
   TOKEN_KEY: "cocktail-critic-client-auth-token"
 };
+
+//https://nameless-taiga-57862.herokuapp.com/
+
+//http://localhost:8000/api
