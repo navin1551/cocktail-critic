f6309f31b8bd1ed68c9339f0ba2777a697b6c5e5
Navin Kumar <navin1551@gmail.com>
2020-03-02 21:44:43 -0800

    added sign-in button functionality

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./NotFoundPage.css";
+import React from 'react';
+import './NotFoundPage.css';
 
 export default class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <section role="contentinfo">
-        <div className="not-found-page-area">
-          <p>Page Not Found!</p>
-        </div>
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section role="contentinfo">
+				<div className="not-found-page-area">
+					<p>Page Not Found!</p>
+				</div>
+			</section>
+		);
+	}
 }
--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,86 +1,89 @@
-import React from "react";
-import TokenService from "../../services/token-service";
-import AuthApiService from "../../services/auth-api-service";
-import "./SignInForm.css";
+import React from 'react';
+import TokenService from '../../services/token-service';
+import AuthApiService from '../../services/auth-api-service';
+import './SignInForm.css';
 
 export default class SignInForm extends React.Component {
-  static defaultProps = {
-    onLoginSuccess: () => {}
-  };
+	static defaultProps = {
+		onLoginSuccess: () => {}
+	};
 
-  state = { error: null };
+	state = { error: null, buttonDisabled: false };
 
-  loginSubmitHandle = e => {
-    const user_name = e.target.children[1];
-    const password = e.target.children[4];
+	loginSubmitHandle = (e) => {
+		const user_name = e.target.children[1];
+		const password = e.target.children[4];
 
-    TokenService.saveAuthToken(
-      TokenService.makeBasicAuthToken(user_name.value, password.value)
-    );
+		TokenService.saveAuthToken(TokenService.makeBasicAuthToken(user_name.value, password.value));
 
-    user_name.value = "";
-    password.value = "";
-    this.props.onLoginSuccess();
-  };
+		user_name.value = '';
+		password.value = '';
+		this.props.onLoginSuccess();
+	};
 
-  handleSubmitJwtAuth = ev => {
-    ev.preventDefault();
-    this.setState({ error: null });
+	handleSubmitJwtAuth = (ev) => {
+		ev.preventDefault();
+		this.setState({ error: null });
 
-    const { user_name, password } = ev.target;
+		//disable sign in button
+		if (this.state.buttonDisabled) return;
+		this.setState({ buttonDisabled: true });
 
-    AuthApiService.postLogin({
-      user_name: user_name.value,
-      password: password.value
-    })
-      .then(res => {
-        user_name.value = "";
-        password.value = "";
-        TokenService.saveAuthToken(res.authToken);
-        this.props.onLoginSuccess();
-        window.location = "/posted-reviews";
-      })
-      .catch(res => {
-        this.setState({ error: res.error });
-      });
-  };
+		const { user_name, password } = ev.target;
 
-  render() {
-    const { error } = this.state;
-    return (
-      <section role="contentinfo">
-        <h3 id="sign-in-title">Already have an account?</h3>
-        <form onSubmit={e => this.handleSubmitJwtAuth(e)}>
-          <label htmlFor="user-name" id="login-user-name-label">
-            User Name:
-          </label>
-          <br />
-          <input type="text" id="login-user-name-input" name="user_name" />
-          <br />
-          <label htmlFor="password" id="login-password-label">
-            Password:
-          </label>
-          <br />
-          <input type="password" id="password-input" name="password" />
-          <br />
-          <span id="user-password-span">
-            *user name/password are case sensitive
-          </span>
-          <br />
-          <button id="sign-in-button">Sign In</button>
-        </form>
-        <div className="demo-login-area">
-          <p id="demo-login-span">Demo login info:</p>
-          <span>User Name:</span>
-          <span>testuser1</span>
-          <br />
-          <span>Password:</span>
-          <span>Password1$</span>
-        </div>
-        <div className="alert-area" role="alert">
-          {error && <span className="red">{error}</span>}
-        </div>
-      </section>
-    );
-  }
+		AuthApiService.postLogin({
+			user_name: user_name.value,
+			password: password.value
+		})
+			.then((res) => {
+				user_name.value = '';
+				password.value = '';
+				TokenService.saveAuthToken(res.authToken);
+				this.props.onLoginSuccess();
+				window.location = '/posted-reviews';
+			})
+			.catch((res) => {
+				this.setState({ error: res.error, buttonDisabled: false });
+			});
+	};
+
+	render() {
+		const { error } = this.state;
+		let buttonFunc = this.state.buttonDisabled ? 'Verifying...' : 'Sign In';
+		return (
+			<section role="contentinfo">
+				<h3 id="sign-in-title">Already have an account?</h3>
+				<form onSubmit={(e) => this.handleSubmitJwtAuth(e)}>
+					<label htmlFor="user-name" id="login-user-name-label">
+						User Name:
+					</label>
+					<br />
+					<input type="text" id="login-user-name-input" name="user_name" />
+					<br />
+					<label htmlFor="password" id="login-password-label">
+						Password:
+					</label>
+					<br />
+					<input type="password" id="password-input" name="password" />
+					<br />
+					<span id="user-password-span">*user name/password are case sensitive</span>
+					<br />
+					<button id="sign-in-button" disabled={this.state.buttonDisabled}>
+						{buttonFunc}
+					</button>
+				</form>
+				<div className="demo-login-area">
+					<p id="demo-login-span">Demo login info:</p>
+					<span>User Name:</span>
+					<span>testuser1</span>
+					<br />
+					<span>Password:</span>
+					<span>Password1$</span>
+				</div>
+				<div className="alert-area" role="alert">
+					{error && <span className="red">{error}</span>}
+				</div>
+			</section>
+		);
+	}
 }
